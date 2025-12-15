86ceecd52950b3b824ee90874772a37267d33d33
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-16 03:34:01 +0530

    Updated profile

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class ProfileComponent implements OnInit {
+  user: any = null;
+  loading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.user = this.authService.getCurrentUser();
+    if (!this.user) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  
+}
