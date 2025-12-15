d11a535d51bb0706aad1fdbc4aecb9d454d5e6c3
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-15 12:01:35 +0530

    Added authservice

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      environment.authUrl + '/signin', 
+      { username, password },
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      environment.authUrl + '/signup',
+      { username, email, password, role: ['user'] },
+      { ...httpOptions, responseType: 'text' as 'json' } 
+    );
+  }
+}
