a38f11de01accd245dce249f12d9c3f6c01960ef
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-16 00:57:10 +0530

    Updated home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterLink]
+})
+export class HomeComponent implements OnInit {
+  isAuthenticated = false;
+  username = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    if (this.isAuthenticated) {
+      const user = this.authService.getCurrentUser();
+      this.username = user?.username || 'User';
+    }
+  }
+}
