e2f5b9a3550c819cd7babb6404a464f8377cf3eb
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-23 01:23:01 +0530

    Added password-change route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SearchFlightsComponent } from './components/search-flights/search-flights.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BookFlightComponent } from './components/book-flight/book-flight'; 
+import { MyBookingsComponent } from './components/my-bookings/my-bookings';
+import { AdminDashboardComponent } from './components/admin-dashboard/add-flights';
+import { UpdatePasswordComponent } from './components/update-password/update-password';
 
 export const routes: Routes = [
   {
@@ -34,6 +36,16 @@ export const routes: Routes = [
     path: 'book/:flightId', 
     component: BookFlightComponent
   },
+  {
+    path: 'my-bookings', 
+    component: MyBookingsComponent
+  },
+  { path: 'admin-dashboard', 
+    component: AdminDashboardComponent
+  },
+  { path: 'update-password', 
+    component: UpdatePasswordComponent, 
+    canActivate: [AuthGuard] },
   {
     path: '',
     redirectTo: 'home',
