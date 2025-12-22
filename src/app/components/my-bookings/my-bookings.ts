2b0d441c0a1b135fc61f69c7c0ff57c4443e37b7
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-22 16:16:06 +0530

    Added cancellation feature

--- src/app/components/my-bookings/my-bookings.ts
+++ src/app/components/my-bookings/my-bookings.ts
@@ -1,30 +1,40 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; 
 import { CommonModule, DatePipe } from '@angular/common';
-import { FlightService } from '../../services/flight.service';
 import { RouterLink } from '@angular/router';
+import { FlightService } from '../../services/flight.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-my-bookings',
   standalone: true,
   imports: [CommonModule, DatePipe, RouterLink], 
   templateUrl: './my-bookings.html',
-  styleUrl:'./my-bookings.css'
+  styleUrls: ['./my-bookings.css']
 })
 export class MyBookingsComponent implements OnInit {
   
   bookings: any[] = [];
   loading = true;
   error = '';
-  
-  currentUserEmail = 'sonal@example.com'; 
+  currentUserEmail = ''; 
 
   constructor(
     private flightService: FlightService,
+    private authService: AuthService,
     private cd: ChangeDetectorRef 
   ) {}
 
   ngOnInit() {
-    this.loadBookings();
+    const user = this.authService.getUser();
+    if (user && user.email) {
+      console.log('Fetching bookings for:', user.email);
+      this.currentUserEmail = user.email;
+      this.loadBookings();
+    } else {
+      console.error('User email not found.');
+      this.error = 'Please log in to view your bookings.';
+      this.loading = false;
+    }
   }
 
   loadBookings() {
@@ -35,14 +45,12 @@ export class MyBookingsComponent implements OnInit {
         console.log('Bookings loaded:', data);
         this.bookings = data || [];
         this.loading = false;
-    
         this.cd.detectChanges(); 
       },
       error: (err) => {
         console.error('Fetch error:', err);
         this.error = 'Could not load bookings.';
         this.loading = false;
-      
         this.cd.detectChanges();
       }
     });
@@ -50,14 +58,13 @@ export class MyBookingsComponent implements OnInit {
 
   cancelTicket(pnr: string) {
     if (!confirm('Are you sure you want to cancel this ticket?')) return;
-
     this.flightService.cancelBooking(pnr).subscribe({
-      next: (res) => {
+      next: (response) => {
         alert('Ticket Cancelled Successfully');
         this.loadBookings(); 
       },
       error: (err) => {
-        alert('Cancellation Failed');
+        alert('Cancellation Failed: ' + (err.error?.message || 'Server Error'));
       }
     });
   }
